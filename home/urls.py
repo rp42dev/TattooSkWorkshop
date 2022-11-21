7593add49d7b66780799c5e380204b7e97d057b1
Raivis Petrovskis <rapet80@gmail.com>
2022-11-21 21:17:12 +0000

    add robots

--- home/urls.py
+++ home/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
+from django.views.generic import TemplateView
 from django.urls import path, include
 from . import views
 
-
 urlpatterns = [
+    path('robots.txt', TemplateView.as_view(
+        template_name="robots.txt", content_type='text/plain')),
     path('', views.index, name='home'),
     path('send_email', views.send_email, name='send_email'),
 ]
