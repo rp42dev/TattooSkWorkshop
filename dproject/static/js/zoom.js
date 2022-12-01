b52cbb47a269f2261eae28d26e25825b6e1d9a07
Raivis Petrovskis <rapet80@gmail.com>
2022-12-01 00:46:34 +0000

    test touchzoom

--- dproject/static/js/zoom.js
+++ dproject/static/js/zoom.js
@@ -5,8 +5,6 @@ var scale = 1,
     start = { x: 0, y: 0 },
     zooms = document.getElementsByClassName("zoom")
 
-console.log(zooms)
-
 for (var i = 0; i < zooms.length; i++) {
     zoom = zooms[i];
     setTransform();
@@ -76,6 +74,79 @@ for (var i = 0; i < zooms.length; i++) {
     zoom.ontouchleave = function (e) {
         panning = false;
     }
+
+    zoom.ontouchenter = function (e) {
+        panning = false;
+    }
+
+    zoom.ontouchend = function (e) {
+        panning = false;
+    }
+
+    // detect pinch zoom
+    zoom.addEventListener('touchstart', handleTouchStart, false);
+    zoom.addEventListener('touchmove', handleTouchMove, false);
+
+    var xDown = null;
+    var yDown = null;
+
+    function getTouches(evt) {
+        return evt.touches ||             // browser API
+            evt.originalEvent.touches; // jQuery
+    }
+
+    function handleTouchStart(evt) {
+        const firstTouch = getTouches(evt)[0];
+        xDown = firstTouch.clientX;
+        yDown = firstTouch.clientY;
+    }
+
+    function handleTouchMove(evt) {
+        if (!xDown || !yDown) {
+            return;
+        }
+
+        var xUp = evt.touches[0].clientX;
+        var yUp = evt.touches[0].clientY;
+
+        var xDiff = xDown - xUp;
+        var yDiff = yDown - yUp;
+
+        if (Math.abs(xDiff) > Math.abs(yDiff)) {
+            if (xDiff > 0) {
+                /* left swipe */
+            } else {
+                /* right swipe */
+            }
+        } else {
+            if (yDiff > 0) {
+                /* up swipe */
+                var xs = (evt.touches[0].clientX - pointX) / scale,
+                    ys = (evt.touches[0].clientY - pointY) / scale,
+                    delta = (evt.wheelDelta ? evt.wheelDelta : -evt.deltaY);
+                (delta > 0) ? (scale *= 1.2) : (scale /= 1.2);
+                pointX = evt.touches[0].clientX - xs * scale;
+                pointY = evt.touches[0].clientY - ys * scale;
+
+                setTransform();
+            } else {
+                /* down swipe */
+                var xs = (evt.touches[0].clientX - pointX) / scale,
+                    ys = (evt.touches[0].clientY - pointY) / scale,
+                    delta = (evt.wheelDelta ? evt.wheelDelta : -evt.deltaY);
+                (delta > 0) ? (scale *= 1.2) : (scale /= 1.2);
+                pointX = evt.touches[0].clientX - xs * scale;
+                pointY = evt.touches[0].clientY - ys * scale;
+
+                setTransform();
+            }
+        }
+        /* reset values */
+        xDown = null;
+        yDown = null;
+    }
+
+
 
 }
 
