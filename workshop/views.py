2cd64497e57fed7b3771bcc81107738b4cdeca4c
Raivis Petrovskis <rapet80@gmail.com>
2022-12-08 00:00:12 +0000

    add prices page

--- workshop/views.py
+++ workshop/views.py
@@ -3,17 +3,19 @@ from django.shortcuts import render
 from django.conf import settings
 from .models import Faq
 from home.models import Page, Section, Article, Image, Video, Element
+from django.utils.translation import gettext_lazy as _
+from django.utils.translation import to_locale, get_language
 
 
 class PageDetailView(DetailView):
     """ A view to show individual image with details """
     model = Page
-
+    
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         if self.request.htmx:
             context['section'] = Section.objects.get(
-                title=self.request.GET['name'])
+                id=self.request.GET['name'])
             context['articles'] = Article.objects.filter(
                 section=context['section'])
             if Image.objects.filter(articles__in=context['articles']).exists():
@@ -25,15 +27,15 @@ class PageDetailView(DetailView):
             return context
         else:
             context['sections'] = Section.objects.filter(page=self.object)
+            print(self.object.name)
             return context
 
     def get_template_names(self):
+        page_name = self.object.name
         if self.request.htmx:
-            return 'includes/about_items.html'
-        else:
-            return 'pages/about.html'
-    
-    
+            return f"includes/{page_name}_items.html"
+        return 'pages/about.html'
+
 
 def faq(request):
     """A view to return the faq page"""
