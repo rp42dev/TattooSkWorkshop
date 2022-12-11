818c79bf725f20046733591d6d5e0b12328d476d
Raivis Petrovskis <rapet80@gmail.com>
2022-12-11 17:32:28 +0000

    fix page

--- config/urls.py
+++ config/urls.py
@@ -20,7 +20,7 @@ urlpatterns = [
     re_path(r'^i18n/', include('django.conf.urls.i18n')),
     re_path(r'^admin/', admin.site.urls),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps},
-         name='django.contrib.sitemaps.views.sitemap')
+         name='django.contrib.sitemaps.views.sitemap'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
