9002b0e6cbf3d146245eeae6ffbe1edfca383b5b
Raivis Petrovskis <rapet80@gmail.com>
2023-01-01 22:44:24 +0000

    fixed image loading

--- dproject/static/js/script.js
+++ dproject/static/js/script.js
@@ -1,69 +1,24 @@
+document.body.addEventListener('htmx:afterSwap', function (event) {
 
-// // Lazy load images
-// $(window).on("load", function () {
-//     $(window).scroll(function () {
-//         var windowBottom = $(this).scrollTop() + $(this).innerHeight();
-//         $(".fade").each(function () {
-//             var objectBottom = $(this).offset().top + 50;
-//             if (objectBottom < windowBottom) {
-//                 $(this).removeClass('fade');
-//             }
-//         });
-//     }).scroll();
-// });
+    var lazyImages = [].slice.call(document.querySelectorAll("img.lazy"));
+    if ("IntersectionObserver" in window) {
+        let lazyImageObserver = new IntersectionObserver(function (entries, observer) {
+            entries.forEach(function (entry) {
+                if (entry.isIntersecting) {
+                    let lazyImage = entry.target;
+                    lazyImage.src = lazyImage.dataset.src;
+                    lazyImage.srcset = lazyImage.dataset.srcset;
+                    lazyImage.classList.remove("lazy");
+                    lazyImageObserver.unobserve(lazyImage);
+                }
+            });
+        });
+        lazyImages.forEach(function (lazyImage) {
+            lazyImageObserver.observe(lazyImage);
+        });
+    } else {
+        // Possibly fall back to a more compatible method here
 
+    }
 
-function initFade() {
-    gsap.registerPlugin(ScrollTrigger);
-
-    gsap.utils.toArray(["article"]).forEach((panel) => {
-        panel.classList.add("fade");
-    });
-
-    gsap.utils.toArray(".fade").forEach((panel) => {
-        gsap.timeline({
-            scrollTrigger: {
-                trigger: panel,
-                start: "top bottom",
-                end: "bottom",
-                scrub: 2,
-                // markers: true,
-            },
-        })
-            .fromTo(panel, {
-                opacity: 0,
-                y: panel.offsetHeight / 10
-            }, {
-                opacity: 1,
-                y: 0
-            })
-    });
-}
-
-$(window).on("load", function () {
-
-    gsap.registerPlugin(ScrollTrigger);
-    
-    gsap.fromTo(".masthead-content h1", {
-        opacity: 0,
-        scale: 0.9,
-    }, {
-        opacity: 0.8,
-        scale: 1,
-        duration: 0.5,
-        ease: "back.out(1.7)",
-    });
-    
-    gsap.fromTo(".slogan h2", {
-        opacity: 0,
-        scale: 0.9,
-    }, {
-        delay: 0.2,
-        opacity: 0.8,
-        scale: 1,
-        duration: 0.5,
-        ease: "back.out(1.7)",
-    });
-
-    initFade();
 });
